40217c4273fedc8026ce5d09870581f4d2074846
Nazrul Islam Rakib <88043438+TigerRakib@users.noreply.github.com>
2025-02-15 20:55:02 +0600

    Update settings.py

--- my_folio/settings.py
+++ my_folio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-gcg)##z44&!#%2wbu09t9siv#=m@tpd7xgbb2&i=x4bgu5e0)q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -119,7 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
